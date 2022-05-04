4edced3b317e0a39460294b2d1e46997f63bdbbc
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2022-05-05 00:24:39 +0430

    question_1

---
+++ JS Questions/script1.js
@@ -0,0 +1,12 @@
+// 1-Create a function that takes in an array and returns an array of the accumulating
+// sum
+// accumulatingArray([1,2,3,4]) =>[1,3,6,10]
+
+function accumulatingArray(array) {
+  let first = 0;
+  return array.map((element) => (first += element));
+}
+console.log("********Question 1********");
+console.log(accumulatingArray([1, 2, 3, 4])); //[1, 3, 6, 10]
+console.log(accumulatingArray([1, 0, 1, 0, 1])); //[1, 1, 2, 2, 3]
+console.log(accumulatingArray([])); //[]
